b2616fb2ab851082ed7b3a7b88e826d058c56db5
Chinar Joshi <chijoshi@tesla.com>
2026-02-23 21:32:34 -0800

    get rid of claude-code custom.el

--- init.el
+++ init.el
@@ -14,18 +14,3 @@
       (load el-file nil t))))
 
 (my/load-modules)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
- '(package-vc-selected-packages
-   '((claude-code :url "https://github.com/stevemolitor/claude-code.el"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
